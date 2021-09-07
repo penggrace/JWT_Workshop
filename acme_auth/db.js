0b1aa9378440996169a157be639c882b30e49e50
Brandy Mak <bmak789@gmail.com>
2021-09-07 13:07:04 -0400

    before lunch changes

---
+++ acme_auth/db.js
@@ -0,0 +1,85 @@
+const Sequelize = require("sequelize");
+//Require jwt in the database file*
+const jwt = require("jsonwebtoken");
+
+//Not sure? ****************
+
+const tokenSecret = process.env.JWT;
+
+const { STRING } = Sequelize;
+const config = {
+  logging: false,
+};
+
+if (process.env.LOGGING) {
+  delete config.logging;
+}
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_db",
+  config
+);
+
+const User = conn.define("user", {
+  username: STRING,
+  password: STRING,
+});
+
+User.byToken = async (token) => {
+  try {
+    // const user = await User.findByPk(token);
+    const verifiedToken = jwt.verify(token, tokenSecret);
+    const user = await User.findByPk(verifiedToken.id);
+    if (user) {
+      return user;
+    }
+    const error = Error("bad credentials");
+    error.status = 401;
+    throw error;
+  } catch (ex) {
+    const error = Error("bad credentials");
+    error.status = 401;
+    throw error;
+  }
+};
+
+User.authenticate = async ({ username, password }) => {
+  const user = await User.findOne({
+    where: {
+      username,
+      password,
+    },
+  });
+  if (user) {
+    console.log("TOKEN SECRET--->", process.env.JWT);
+    return jwt.sign({ id: user.id, username: user.username }, tokenSecret);
+  }
+  const error = Error("bad credentials");
+  error.status = 401;
+  throw error;
+};
+
+const syncAndSeed = async () => {
+  await conn.sync({ force: true });
+  const credentials = [
+    { username: "lucy", password: "lucy_pw" },
+    { username: "moe", password: "moe_pw" },
+    { username: "larry", password: "larry_pw" },
+  ];
+  const [lucy, moe, larry] = await Promise.all(
+    credentials.map((credential) => User.create(credential))
+  );
+  return {
+    users: {
+      lucy,
+      moe,
+      larry,
+    },
+  };
+};
+
+module.exports = {
+  syncAndSeed,
+  models: {
+    User,
+  },
+};
